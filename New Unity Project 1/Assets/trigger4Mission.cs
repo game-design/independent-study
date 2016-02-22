fae8016f283900606def663f8ed1c4ada022b7f0
wawjchinawct <wawjchinawct@gmail.com>
2016-02-22 14:01:51 -0500

    Popup menu

--- New Unity Project 1/Assets/trigger4Mission.cs
+++ New Unity Project 1/Assets/trigger4Mission.cs
@@ -4,21 +4,21 @@ using System.Collections;
 public class trigger4Mission : MonoBehaviour {
 
     public bool windowSwitch = false;
-    private Rect windowRect = new Rect(200, 80, 240, 100);
+    private Rect windowRect = new Rect(200, 20, 200, 100);
 
     void OnGUI()
     {
         if (windowSwitch == true)
         {
-            windowRect = GUI.Window(0, windowRect, WindowContain, "测试视窗");
+            windowRect = GUI.Window(0, windowRect, WindowContain, "  Left is AA \n Right is BB");
         }
     }
     public void WindowContain(int windowID)
     {
-        if (GUI.Button(new Rect(70, 40, 100, 20), "关闭视窗"))
+        /*if (GUI.Button(new Rect(70, 40, 100, 20), "关闭视窗"))
         {
             windowSwitch = false;
-        }
+        }*/
     }
 
 
@@ -38,6 +38,11 @@ public class trigger4Mission : MonoBehaviour {
     void OnTriggerEnter(Collider other)
     {
         windowSwitch = true;
+        //Debug.Log(other.gameObject.transform.position.x);
+    }
+    void OnTriggerExit(Collider other)
+    {
+        windowSwitch = false;
         //Debug.Log(other.gameObject.transform.position.x);
     }
 
