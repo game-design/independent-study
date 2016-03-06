6e6b90079d427608ed5f9bc6933a6e9cec1ebac2
wawjchinawct <wawjchinawct@gmail.com>
2016-03-05 19:39:33 -0500

    Fix Timescale

--- New Unity Project 1/Assets/script/PauseMenu.cs
+++ New Unity Project 1/Assets/script/PauseMenu.cs
@@ -7,6 +7,7 @@ public class PauseMenu : MonoBehaviour {
     public string levelToLoad;
     public bool paused = false;
     public string initial_level;
+    int myClickTimer = 20;
 
 	// Use this for initialization
 	void Start () {
@@ -15,11 +16,12 @@ public class PauseMenu : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.Escape))
+        if (Input.GetKey(KeyCode.Escape) && myClickTimer == 0)
         {
-            if (!paused)
-                paused = true;
+            myClickTimer = 20;
+            paused = !paused;
         }
+        if(myClickTimer > 0)    myClickTimer--;
         if (paused)
             Time.timeScale = 0;
         else
@@ -38,11 +40,17 @@ public class PauseMenu : MonoBehaviour {
             }
             if (GUI.Button(new Rect(Screen.width / 4 + 10, Screen.height / 4 + 2 * Screen.height / 10 + 10, Screen.width / 2 - 20, Screen.height / 10), "RESTART"))
             {
+                //Time.timeScale = 1;
+                paused = false;
                 SceneManager.LoadScene(initial_level);
             }
             if (GUI.Button(new Rect(Screen.width / 4 + 10, Screen.height / 4 + 3 * Screen.height / 10 + 10, Screen.width / 2 - 20, Screen.height / 10), "RESET"))
             {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                //Time.timeScale = 1;
+                paused = false;
+                Scene scene = SceneManager.GetActiveScene();
+                SceneManager.LoadScene(scene.name);
+                //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
         }
     }
