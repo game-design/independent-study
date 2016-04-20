54e36372e7b8a201eafc2541d46d6604f5bdd6dd
dingfs <dingfengshao@gmail.com>
2016-04-20 09:39:46 -0400

    start animation

---
+++ New Unity Project 1/Assets/script/startAnimationControl.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startAnimationControl : MonoBehaviour {
+
+    Rigidbody playerRigidbody;
+    Animation anim;
+    Vector3 movement;
+    public Transform leftPoint;
+    public Transform rightPoint;
+    bool left;
+    // Use this for initialization
+    void Start () {
+        playerRigidbody = GetComponent<Rigidbody>();
+        anim = GetComponent<Animation>();
+        playerRigidbody.position = leftPoint.position;
+        left = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (left)
+        {
+            movement.Set(1f, 0f, 0f);
+            movement = movement.normalized * 45f * Time.deltaTime;
+            playerRigidbody.MovePosition(transform.position + movement);
+
+            Quaternion newRotation = Quaternion.LookRotation(movement);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+        else
+        {
+            movement.Set(-1f, 0f, 0f);
+            movement = movement.normalized * 45f * Time.deltaTime;
+            playerRigidbody.MovePosition(transform.position + movement);
+
+            Quaternion newRotation = Quaternion.LookRotation(movement);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+
+        if (playerRigidbody.position.x < leftPoint.position.x)
+            left = true;
+        if (playerRigidbody.position.x > rightPoint.position.x)
+            left = false;
+
+	}
+}
