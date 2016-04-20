d816fb04c444bab1796ef787a9a311d00c8c5252
dingfs <dingfengshao@gmail.com>
2016-04-20 11:07:35 -0400

    add camera animation

---
+++ New Unity Project 1/Assets/script/menuManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class menuManager : MonoBehaviour {
+
+
+    public Animator camera_animator;
+
+    public Button credit;
+    public Button back;
+    public Button start;
+    
+
+	// Use this for initialization
+	void Start () {
+        back.gameObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    public void triggerCreditAnimation()
+    {
+        camera_animator.SetTrigger("checkCredit");
+        back.gameObject.SetActive(true);
+        credit.gameObject.SetActive(false);
+        start.gameObject.SetActive(false);
+    }
+
+    public void triggerBackAnimation()
+    {
+        camera_animator.SetTrigger("backIntro");
+        back.gameObject.SetActive(false);
+        credit.gameObject.SetActive(true);
+        start.gameObject.SetActive(true);
+    }
+
+
+
+}
