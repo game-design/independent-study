6c7b4b9ff0d086b08f1a4ae536b3627e0ec5192b
dingfs <dingfengshao@gmail.com>
2016-04-20 23:25:37 -0400

    add fade text

---
+++ New Unity Project 1/Assets/script/FadeText.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeText : MonoBehaviour {
+    public float transitionSpeed = 30;
+
+    float currentGoal = 0;
+    TextMesh textMesh;
+
+
+    void Start()
+    {
+        textMesh = GetComponent<TextMesh>();
+        textMesh.color = new Vector4(textMesh.color.r, textMesh.color.g, textMesh.color.b, currentGoal);
+    }
+
+    void Update()
+    {
+        textMesh.color = Vector4.MoveTowards(textMesh.color, new Vector4(textMesh.color.r, textMesh.color.g, textMesh.color.b, currentGoal), Time.deltaTime * transitionSpeed);
+    }
+
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        TriggerValue value = collider.GetComponent<TriggerValue>();
+        if (value != null)
+        {
+            currentGoal = value.triggerFloat;
+        }
+    }
+}
---
+++ New Unity Project 1/Assets/script/TriggerValue.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerValue : MonoBehaviour {
+    public float triggerFloat;
+
+}
